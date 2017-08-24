0ab86f6503fe2ec56e734f55964b665cc663242a
Carlo <cruiza9@upao.edu.pe>
2017-08-23 19:45:37 -0500

    Creacion de Java  class Alumno

---
+++ app/src/main/java/rac/com/example/upao/upaoselfie/model/Alumno.java
@@ -0,0 +1,38 @@
+package rac.com.example.upao.upaoselfie.model;
+
+import java.util.Date;
+
+/**
+ * Created by Upao on 23/08/2017.
+ */
+
+public class Alumno {
+
+    private String nombre;
+    private long numero_cuenta;
+    private Date fechaNacimiento;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getNumero_cuenta() {
+        return numero_cuenta;
+    }
+
+    public void setNumero_cuenta(long numero_cuenta) {
+        this.numero_cuenta = numero_cuenta;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
